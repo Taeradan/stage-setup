5ee5f1d2c9a4dc195da3c3f2c9bfb64a84b78c31
Yves Dubromelle <yves+git@dubronetwork.fr>
2016-05-08 00:53:51 +0200

    renommage et ajout du mellotron

--- audiodd-routing.py
+++ audiodd-routing.py
@@ -3,12 +3,16 @@ from mididings import *
 config(
         backend = 'jack',
         client_name = 'audiodd_routing',
-        out_ports = ['taurus','polybass','calfcontrols']
+        out_ports = ['taurus','polybass','mellotron','calfcontrols']
 )
 
 taurus = Print('taurus: ') >> Output('taurus', 1)
 polybass = Print('polybass: ') >> Output('polybass', 1)
+mellotron = Print('mellotron: ') >> Output('mellotron', 1)
+
+pk5 = taurus
+clavier = KeySplit('c2', polybass, mellotron)
 
 run(
-        patch = ChannelSplit({1: taurus, 2: polybass})
+        patch = ChannelSplit({1: pk5, 2: clavier})
 )
